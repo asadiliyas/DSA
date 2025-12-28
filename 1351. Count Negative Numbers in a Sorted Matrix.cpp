de4edf0b0cc31dc281973e6115c8d8de89c6fe8b
Asad Iliyas <141337715+asadiliyas@users.noreply.github.com>
2025-12-28 20:45:51 +0530

    Add function to count negative numbers in matrix
    Implement a function to count negative numbers in a sorted matrix.

---
+++ 1351. Count Negative Numbers in a Sorted Matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    // TC = O(m*n)
+    // SC = O(1)
+
+    int countNegatives(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(grid[i][j] < 0){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
